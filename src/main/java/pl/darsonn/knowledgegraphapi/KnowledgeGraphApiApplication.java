ed994b2c3b293ad6f45a3132889b187b0dd4f8d4
Darsonn <87154431+Darsonn@users.noreply.github.com>
2026-02-04 15:37:20 +0100

    Initial commit

---
+++ src/main/java/pl/darsonn/knowledgegraphapi/KnowledgeGraphApiApplication.java
@@ -0,0 +1,13 @@
+package pl.darsonn.knowledgegraphapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeGraphApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeGraphApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/darsonn/knowledgegraphapi/KnowledgeGraphApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.darsonn.knowledgegraphapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeGraphApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
