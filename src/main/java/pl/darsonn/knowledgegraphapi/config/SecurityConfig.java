f0827b911f4d0d610ebb6fd8fc0a60eb8a5695d1
Darsonn <87154431+Darsonn@users.noreply.github.com>
2026-02-04 20:16:37 +0100

    Hotfix - removed requiresChannel inside SecurityConfig.java

--- src/main/java/pl/darsonn/knowledgegraphapi/config/SecurityConfig.java
+++ src/main/java/pl/darsonn/knowledgegraphapi/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -20,17 +21,16 @@ public class SecurityConfig {
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http
-                .csrf(csrf -> csrf.disable()) // Wyłączone, bo używamy JWT (brak sesji)
-                .requiresChannel(channel -> channel.anyRequest().requiresSecure()) // Wymusza HTTPS
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll() // Logowanie i rejestracja są otwarte
-                        .requestMatchers("/login/oauth2/**").permitAll() // Google Login otwarty
-                        .anyRequest().authenticated() // Wszystko inne wymaga tokena
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/login/oauth2/**").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Brak sesji na serwerze
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
