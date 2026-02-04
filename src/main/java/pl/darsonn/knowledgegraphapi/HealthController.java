2f606f2a33f4eee8cbc401b2a0cbc8cbc010f46a
Darsonn <87154431+Darsonn@users.noreply.github.com>
2026-02-04 19:17:39 +0100

    Changed path to health request

--- src/main/java/pl/darsonn/knowledgegraphapi/HealthController.java
+++ src/main/java/pl/darsonn/knowledgegraphapi/HealthController.java
@@ -1,15 +1,15 @@
 package pl.darsonn.knowledgegraphapi;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/health")
 class HealthController {
-    @GetMapping()
-    HttpStatus returnHealthy() {
-        return HttpStatus.OK;
+    @GetMapping
+    ResponseEntity<Void> returnHealthy() {
+        return ResponseEntity.ok().build();
     }
 }
